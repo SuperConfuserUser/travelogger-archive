b4f92d98e0aa76a812f77fa876d5c414579aa0a3
Chely Ho <adept_inept@yahoo.com>
2018-05-06 00:20:56 +0000

    seeded db with trip categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+trip_categories = ['Business', 'Holiday', 'Activity', 'Other']
+trip_categories.each {|c| TripCategory.create(name: c)}
