7e64f979d8fdfcb8a43bbe4ed35a749f857ee130
Chely Ho <adept_inept@yahoo.com>
2018-05-06 03:47:08 +0000

    blocked out user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+  before_action :new_user, only [:new, :create]
+  before_action :set_user, only: [:show, :edit]
+  #before_action :user, only: [:new, :create, :show, :edit]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  private
+
+  def new_user
+    @user = User.new
+  end
+
+  def set_user
+   # nice to have: slugifiable instead of id
+   @user = User.find(:id)
+  end
+
+  # need to test
+  def user
+    @user = User.find(:id) || User.new
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation, :about)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password   #password and password_confirmation
 
   has_many :trips
 end
